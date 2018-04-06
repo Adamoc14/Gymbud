da2ba6d0c570ac8fcf4380265a9ac4779a307222
adamoc <oceallaighadam96@gmail.com>
2018-04-06 14:56:22 +0100

    this is the new project-Gymbud

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home | Gymbud</title>
+</head>
+<body>
+	<ul>
+		<li>Home</li>
+		<li>Home</li>
+		<li>Home</li>
+		<li>Home</li>
+		<li>Home</li>
+		<li>Home</li>
+		<li>Home</li>
+		<li>Home</li>
+		 
+	</ul>
+</body>
+</html>
