6ed8e71eb8bc30ee208d5575dd60350ee8e799eb
adamoc <oceallaighadam96@gmail.com>
2018-04-07 08:41:16 +0100

    User Login Page Half Done

--- Loginhomepage.php
+++ Loginhomepage.php
@@ -1,7 +1,52 @@
-<?php include('header.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title> Login | Gymbud</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Calligraffitti|Delius|Give+You+Glory|La+Belle+Aurore|Oxygen+Mono|Schoolbell|Sirin+Stencil|Sue+Ellen+Francisco|The+Girl+Next+Door|Erica+One|Modak|Sigmar+One|Indie Flower|Yellowtail|Satisfy|Rancho|Dancing Script|VT323" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="screen" href="Resources/CSS/Login.css" />
+    <script src="Resources/JS/.js"></script>
+</head>
+<body>
 
 
-<?php 
-
-echo "Welcome to Gymbud";
+<?php
+    include('header.php');
 ?>
+
+
+
+
+
+    <div id="inputContainer">
+        <div id="WelcomeMessage">
+            <h1>Welcome to Gymbud</h1>
+            <div id="Gymbud_logoContainer">
+                <img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/819/712153819_c8f6ea8e-7948-4517-a3ac-1587e557ef6e.png?cb=1523085120" id="Gymbud_logo">
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        
+        <div id="LoginInfo">
+            <form action="LoginHomepage.php" method="post">
+                <label for="UsersUsername">Username:</label>
+                <input type="text" name="User_Username" id="UsersUsername" maxlength="30" placeholder="Username or Email" autocorrect="off" autocomplete="off" autocapitalize="off"  aria-describedby="" aria-label="Username or Email" aria-required="true"  value="" required>
+                <br>
+                <br>
+                <br>
+               <label for="UsersPassword">Password:</label>
+                <input type="password" name="User_Password" id="UsersPassword" maxlength="20"  placeholder="Password"  autocorrect="off" autocomplete="off" autocapitalize="off"  aria-describedby="" aria-label="Password" aria-required="true"  value="" required>
+                <br>
+            </form>
+            <input type="submit" id="UserLoginButton" value="Login">
+        </div>
+    </div>
+
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Calligraffitti|Delius|Give+You+Glory|La+Belle+Aurore|Oxygen+Mono|Schoolbell|Sirin+Stencil|Sue+Ellen+Francisco|The+Girl+Next+Door|Erica+One|Modak|Sigmar+One|Indie Flower|Yellowtail|Satisfy|Rancho|Dancing Script|VT323" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="screen" href="Resources/CSS/Header.css" />
+    <script src="Resources/JS/.js"></script>
 	<title>Home | Gymbud</title>
 </head>
 <body>
-
-	<header>
-		<img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/660/687639660_b4c46df1-88c5-4e4a-8ee5-8ee7a3839519.png?cb=1522287464">
-<ul>
-	<li><a href="">Home</a></li>
-	<li><a href="">About</a></li>
-	<li><a href="">Shop</a></li>
-</ul>
-	</header>
+	<div class="container">
+        <div class="heading">
+			   <img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/688/712144688_ad4f3754-6361-4e36-9d53-9c05937cf79b.png?cb=1523084620" >
+        </div>
+        <ul>
+            <li><a href = "#">Home</a></li>
+            <li><a href = "#">Home</a></li>
+            <li><a href = "#">Home</a></li>
+            <li><a href = "#">Home</a></li>
+        </ul>
+    </div>  
+	
 </body>
 </html>
